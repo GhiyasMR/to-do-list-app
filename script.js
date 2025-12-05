99d12d4c600af6c714c930e7c39747222a5abbdb
GhiyasMR <rasyidg28@gmail.com>
2025-12-05 09:17:06 +0700

    added UI changes functionality

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ function showOrCloseModal() {
     addTaskSection.classList.toggle("hidden");
 }
 
-const quadrant1Task = [];
-const quadrant2Task = [];
-const quadrant3Task = [];
-const quadrant4Task = [];
+const quadrant1Task = JSON.parse(localStorage.getItem("quadrant1")) || [];
+const quadrant2Task = JSON.parse(localStorage.getItem("quadrant2")) || [];
+const quadrant3Task = JSON.parse(localStorage.getItem("quadrant3")) || [];
+const quadrant4Task = JSON.parse(localStorage.getItem("quadrant4")) || [];
 
 function addTask() {
     if (titleInput.value === "") {
@@ -29,24 +29,64 @@ function addTask() {
 
     if (urgencyCheckbox.checked && importanceCheckbox.checked) {
         quadrant1Task.push(titleInput.value);
-        localStorage.setItem("quadrant1", JSON.stringify(quadrant1Task));
     } else if (importanceCheckbox.checked) {
         quadrant2Task.push(titleInput.value);
-        localStorage.setItem("quadrant2", JSON.stringify(quadrant2Task));
     } else if (urgencyCheckbox.checked) {
         quadrant3Task.push(titleInput.value);
-        localStorage.setItem("quadrant3", JSON.stringify(quadrant3Task));
     } else {
         quadrant4Task.push(titleInput.value);
-        localStorage.setItem("quadrant4", JSON.stringify(quadrant4Task));
     }
+
+    localStorage.setItem("quadrant1", JSON.stringify(quadrant1Task));
+    localStorage.setItem("quadrant2", JSON.stringify(quadrant2Task));
+    localStorage.setItem("quadrant3", JSON.stringify(quadrant3Task));
+    localStorage.setItem("quadrant4", JSON.stringify(quadrant4Task));
 
     titleInput.value = "";
     urgencyCheckbox.checked = false;
     importanceCheckbox.checked = false;
 }
 
+function changeUI() {
+    let htmlElement1 = "<h2>DO IT</h2>";
+    quadrant1Task.forEach(task => {
+        htmlElement1 += `
+            <label><input type="checkbox" class="tasks"> ${task}</label><br>
+        `
+    })
+    quadrant1.innerHTML = htmlElement1.trim();
+    
+    let htmlElement2 = "<h2>DO IT</h2>";
+    quadrant2Task.forEach(task => {
+        htmlElement2 += `
+            <label><input type="checkbox" class="tasks"> ${task}</label><br>
+        `
+    })
+    quadrant2.innerHTML = htmlElement2.trim();
+
+    let htmlElement3 = "<h2>DO IT</h2>";
+    quadrant3Task.forEach(task => {
+        htmlElement3 += `
+            <label><input type="checkbox" class="tasks"> ${task}</label><br>
+        `
+    })
+    quadrant3.innerHTML = htmlElement3.trim();
+
+    let htmlElement4 = "<h2>DO IT</h2>";
+    quadrant4Task.forEach(task => {
+        htmlElement4 += `
+            <label><input type="checkbox" class="tasks"> ${task}</label><br>
+        `
+    })
+    quadrant4.innerHTML = htmlElement4.trim();
+}
+
 addTaskButton.addEventListener("click", addTask);
 
 showAddTask.addEventListener("click", showOrCloseModal);
-closeAddTask.addEventListener("click", showOrCloseModal);
+closeAddTask.addEventListener("click", () => {
+    showOrCloseModal();
+    changeUI();
+});
+
+document.addEventListener("DOMContentLoaded", changeUI)
